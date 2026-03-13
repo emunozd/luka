a1004f9e18f3f6452883288442265d664f071578
emunozd <elkin.dmd@gmail.com>
2026-03-13 14:25:46 -0500

    depuración proyecto

--- app/core/config.py
+++ app/core/config.py
@@ -7,9 +7,8 @@ class Settings(BaseSettings):
     postgres_password: str
     postgres_host: str = "luka-postgres"
     postgres_port: int = 5432
-    #mlx_server_url: str = "http://host.docker.internal:8181/luka"
-    mlx_server_url: str = "http://192.168.0.90:8181/luka"
-    jwt_secret: str = "cambia_esto_en_produccion"
+    mlx_server_url: str = "http://0.0.0.0:8181/luka"
+    jwt_secret: str = "change_me"
     jwt_algorithm: str = "HS256"
     jwt_expire_minutes: int = 60 * 24 * 7  # 7 días
     brevo_api_key: str
